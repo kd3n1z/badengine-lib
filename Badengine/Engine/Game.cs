bedd48edc0b94c8e7746eb35f42a9e353fa37644
Денис Комарьков <denis.komarkov@gmail.com>
2023-10-27 03:53:47 +0300

    refactor: rename GameWindows to Game & make it static

---
+++ Badengine/Engine/Game.cs
@@ -0,0 +1,34 @@
+﻿using static Raylib_cs.Raylib;
+
+namespace Badengine.Engine;
+
+public static class Game {
+    private static Scene? _scene;
+
+    public static void LoadScene(Scene scene) {
+        _scene = scene;
+    }
+
+    public static void Run(string title) {
+        InitWindow(800, 450, title);
+
+        while (!WindowShouldClose()) {
+            Graphics.BeginDrawing();
+
+            if (_scene == null) {
+                Graphics.UI.DrawText("no scene set", 400, 225, new Color(255, 255, 255), 32,
+                    Graphics.UI.TextOrigin.Center);
+            }
+            else {
+                _scene.Start();
+                _scene.Update();
+
+                _scene.Render();
+            }
+
+            Graphics.EndDrawing();
+        }
+
+        CloseWindow();
+    }
+}
